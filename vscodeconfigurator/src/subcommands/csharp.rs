2f1d942984bfe8cd64b950402e4f8c04d181e2bc
Timothy Small <smalls.online@icloud.com>
2024-09-26 14:28:23 -0400

    Update csharp subcommands

--- vscodeconfigurator/src/subcommands/csharp.rs
+++ vscodeconfigurator/src/subcommands/csharp.rs
@@ -1,23 +1,30 @@
 pub mod init;
 
 use clap::Subcommand;
+use init::InitCommandArgs;
 
+/// Subcommands for C# projects.
 #[derive(Subcommand)]
 #[command(
     about = "Commands for creating and managing C# projects.",
     arg_required_else_help = true,
 )]
 pub enum CsharpSubcommands {
+    /// Initialize a new C# project.
     #[command(
         about = "Initialize a new C# project.",
-        long_about = "Initialize a new C# project."
+        long_about = None
     )]
-    Init(init::InitArgs)
+    Init(InitCommandArgs)
 }
 
-pub fn match_subcommand(subcommand: &Option<CsharpSubcommands>) {
-    match subcommand {
-        Some(CsharpSubcommands::Init(init_args)) => init::run_command(init_args),
-        None => println!("No csharp subcommand provided")
+impl CsharpSubcommands {
+    /// Matches the subcommand provided by the user and runs the corresponding command.
+    pub fn match_subcommand(&self) {
+        match self {
+            CsharpSubcommands::Init(init_args) =>
+                InitCommandArgs::run_command(init_args)
+                    .expect("Failed to run 'csharp init' command.")
+        }
     }
 }
