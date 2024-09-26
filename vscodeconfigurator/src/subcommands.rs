957c5c3f104a115ffb4bfe25211f7c1a52880ac2
Timothy Small <smalls.online@icloud.com>
2024-09-26 14:28:36 -0400

    Add doc comments

--- vscodeconfigurator/src/subcommands.rs
+++ vscodeconfigurator/src/subcommands.rs
@@ -3,8 +3,10 @@ pub mod csharp;
 use clap::Subcommand;
 use csharp::CsharpSubcommands;
 
+/// The root subcommands for the CLI.
 #[derive(Subcommand)]
 pub enum RootSubcommands {
+    /// Subcommands for C# projects.
     Csharp {
         #[command(subcommand)]
         command: Option<CsharpSubcommands>
