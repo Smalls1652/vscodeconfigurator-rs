c3c39500b14d68a407754e4d0237ab933ab41506
Timothy Small <smalls.online@icloud.com>
2024-09-27 23:34:37 -0400

    Update utils

--- vscodeconfigurator/src/utils.rs
+++ vscodeconfigurator/src/utils.rs
@@ -1,5 +1,12 @@
-use std::{env, path::PathBuf};
+use std::{env, ffi::OsString, path::PathBuf};
 
+/// Gets the default value for the `output_directory` (`--output-directory`) argument if 
+/// it is not provided by the user.
+pub fn get_output_directory_default_value() -> OsString {
+    env::current_dir().unwrap().into_os_string()
+}
+
+/// Gets the path to the core templates directory.
 pub fn get_core_templates_path() -> PathBuf {
     let binary_path = env::current_exe().unwrap();
 
