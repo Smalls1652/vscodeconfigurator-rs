a87d03e55fb6b10c787ce658d6ce6fb7ee247123
Timothy Small <smalls.online@icloud.com>
2024-10-01 11:37:27 -0400

    Add VSCodeSettings struct
    This struct acts as a generic wrapper for interacting with the `settings.json` file for VSCode workspaces.

--- vscodeconfigurator/src/vscode_ops/csharp.rs
+++ vscodeconfigurator/src/vscode_ops/csharp.rs
@@ -2,7 +2,11 @@ use std::{fs, path::PathBuf};
 
 use serde_json::{json, Value};
 
-use crate::{console_utils::ConsoleUtils, subcommands::csharp::init::CsharpLspOption};
+use crate::{
+    console_utils::ConsoleUtils,
+    subcommands::csharp::init::CsharpLspOption,
+    vscode_ops::VSCodeSettings
+};
 
 /// Updates the C# LSP option in the `.vscode/settings.json` file.
 /// 
@@ -16,26 +20,21 @@ pub fn update_csharp_lsp(
     csharp_lsp: CsharpLspOption,
     console_utils: &mut ConsoleUtils
 ) -> Result<(), Box<dyn std::error::Error>> {
-    let vscode_settings_path = output_directory.join(".vscode/settings.json");
-
     console_utils.write_info(format!("- 📄 Updating C# LSP option in tasks.json... "))?;
 
-    let vscode_settings_json = fs::read_to_string(&vscode_settings_path)?;
+    let mut vscode_settings = VSCodeSettings::new(output_directory.join(".vscode/settings.json"));
 
-    let mut vscode_settings: Value = serde_json::from_str(&vscode_settings_json.as_str())?;
-
-    vscode_settings["dotnet.server.useOmnisharp"] = match csharp_lsp {
+    vscode_settings.values["dotnet.server.useOmnisharp"] = match csharp_lsp {
         CsharpLspOption::CsharpLsp => Value::Bool(false),
         CsharpLspOption::OmniSharp => Value::Bool(true)
     };
 
-    vscode_settings["dotnet.server.path"] = match csharp_lsp {
+    vscode_settings.values["dotnet.server.path"] = match csharp_lsp {
         CsharpLspOption::CsharpLsp => Value::String("".to_string()),
         CsharpLspOption::OmniSharp => Value::String("latest".to_string())
     };
 
-    let updated_vscode_settings_json = serde_json::to_string_pretty(&vscode_settings)?;
-    fs::write(&vscode_settings_path, updated_vscode_settings_json)?;
+    vscode_settings.write_settings()?;
 
     console_utils.write_success(format!("Done! ✅\n"))?;
 
