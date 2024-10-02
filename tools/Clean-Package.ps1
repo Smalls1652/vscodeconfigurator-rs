20e2b80cb77a4128220b26d5628d9364cd68ca06
Timothy Small <smalls.online@icloud.com>
2024-10-02 17:47:19 -0400

    Add Clean-Package.ps1 script

---
+++ tools/Clean-Package.ps1
@@ -0,0 +1,33 @@
+[CmdletBinding()]
+param(
+    [Parameter(Position = 0)]
+    [ValidateSet(
+        "vscodeconfigurator"
+    )]
+    [string]$PackageName = "vscodeconfigurator",
+    [Parameter(Position = 1)]
+    [ValidateSet(
+        "Debug",
+        "Release"
+    )]
+    [string]$BuildProfile = "Debug"
+)
+
+$cargoBuildArgs = [System.Collections.Generic.List[string]]::new()
+
+$cargoBuildArgs.Add("clean")
+$cargoBuildArgs.Add("--package")
+$cargoBuildArgs.Add($PackageName)
+
+if ($BuildProfile -eq "Release") {
+    $cargoBuildArgs.Add("--release")
+}
+
+$cargoBuildProcSplat = @{
+    "FilePath" = "cargo";
+    "ArgumentList" = $cargoBuildArgs;
+    "NoNewWindow" = $true;
+    "Wait" = $true;
+    "ErrorAction" = "Stop";
+}
+Start-Process @cargoBuildProcSplat
