2cd552907dbb8ef4b17d1cc45cc870f148ba270c
Timothy Small <smalls.online@icloud.com>
2024-10-06 21:02:40 -0400

    Add CargoPackageTemplateOption enum

--- vscodeconfigurator/src/subcommands/rust/init.rs
+++ vscodeconfigurator/src/subcommands/rust/init.rs
@@ -57,3 +57,15 @@ impl RustInitCommandArgs {
         Ok(())
     }
 }
+
+/// The type of Cargo package template to use.
+#[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
+pub enum CargoPackageTemplateOption {
+    /// A binary.
+    #[value(name = "Binary")]
+    Binary,
+
+    /// A library.
+    #[value(name = "Library")]
+    Library,
+}
